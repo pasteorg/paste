ade7c3a3aae4fc74d3295da0dfc964d4123696ee
amk <amk@localhost>
2005-04-22 18:34:13 +0000

    Change application to run an iterable

--- examples/helloworld/index.py
+++ examples/helloworld/index.py
@@ -7,22 +7,25 @@ Automated greeting application.
 import cgi
 
 def application(environ, start_response):
-    form = cgi.FieldStorage(fp=environ['wsgi.input'],
-                            environ=environ,
-                            keep_blank_values=1)
-    start_response('200 OK', [('Content-type', 'text/html')])
+    def body():
+        form = cgi.FieldStorage(fp=environ['wsgi.input'],
+                                environ=environ,
+                                keep_blank_values=1)
 
-    if form.getvalue('name'):
-        yield ('<html><head><title>Hello!</title></head>\n')
-        yield ('<body>\n')
-        yield ('<h1>Hello %s!</h1>\n' % form['name'].value)
-    else:
-        yield ('<html><head><title>Who is there?</title></head>\n')
-        yield ('<body>\n')
-        yield ('<h1>Who is there?</h1>\n')
-    yield ('<form action="%s" method="POST">\n' % environ['SCRIPT_NAME'])
-    yield ('What is your name?<br>\n')
-    yield ('<input type="text" name="name" value="%s"><br>\n'
-          % cgi.escape(form.getvalue('name', ''), 1))
-    yield ('<input type="submit" value="That is my name"></form>\n')
-    yield ('</body></html>\n')
+        if form.getvalue('name'):
+            yield ('<html><head><title>Hello!</title></head>\n')
+            yield ('<body>\n')
+            yield ('<h1>Hello %s!</h1>\n' % form['name'].value)
+        else:
+            yield ('<html><head><title>Who is there?</title></head>\n')
+            yield ('<body>\n')
+            yield ('<h1>Who is there?</h1>\n')
+        yield ('<form action="%s" method="POST">\n' % environ['SCRIPT_NAME'])
+        yield ('What is your name?<br>\n')
+        yield ('<input type="text" name="name" value="%s"><br>\n'
+              % cgi.escape(form.getvalue('name', ''), 1))
+        yield ('<input type="submit" value="That is my name"></form>\n')
+        yield ('</body></html>\n')
+
+    start_response('200 OK', [('Content-type', 'text/html')])
+    return body()
