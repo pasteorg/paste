f5048a14d60fe74d1c7ab0e7edb052b943e0bc44
ianb <ianb@localhost>
2005-04-26 02:10:41 +0000

    Renamed publish_application to publish_app; put in warning about webkit_dir

--- paste/server.py
+++ paste/server.py
@@ -247,13 +247,17 @@ servers['console'] = console_server
 def make_app(conf):
     if conf.get('publish_dir'):
         app = wsgiwebkit.webkit(conf['publish_dir'], use_lint=conf.get('lint'))
-    elif conf.get('publish_application'):
-        app = conf['publish_application']
+    elif conf.get('publish_app'):
+        app = conf['publish_app']
         if isinstance(app, (str, unicode)):
             from paste.util import import_string
             app = import_string.eval_import(app)
     else:
-        print "You must provide publish_dir or publish_application"
+        # @@ ianb 2005-03-23: This should be removed sometime
+        if conf.get('webkit_dir'):
+            print 'The webkit_dir configuration variable is no longer supported'
+            print 'Use publish_dir instead'
+        print "You must provide publish_dir or publish_app"
         sys.exit(2)
     return config_middleware(app, conf)
 
