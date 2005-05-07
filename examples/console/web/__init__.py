993c32267326401aae344b0c6e83616ad208a5fd
ianb <ianb@localhost>
2005-05-07 07:46:56 +0000

    Use validating middleware

--- examples/console/web/__init__.py
+++ examples/console/web/__init__.py
@@ -1,6 +1,11 @@
 import os
 from paste import wsgilib
+from paste import wdg_validate
 
 def urlparser_hook(environ):
     if not environ.has_key('console.base_url'):
         environ['console.base_url'] = environ['SCRIPT_NAME']
+
+def urlparser_wrap(environ, start_response, app):
+    return wdg_validate.WDGValidateMiddleware(app)(
+        environ, start_response)
