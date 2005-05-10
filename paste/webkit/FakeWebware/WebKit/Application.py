436e96d79ba859d45b0b93f0a2284ee6da2dec2d
ianb <ianb@localhost>
2005-05-10 22:21:20 +0000

    Add WebKit.Application; Task manager setup

---
+++ paste/webkit/FakeWebware/WebKit/Application.py
@@ -0,0 +1,1 @@
+from paste.webkit.wkapplication import *
--- paste/webkit/wkapplication.py
+++ paste/webkit/wkapplication.py
@@ -3,6 +3,30 @@ A mostly dummy class to simulate the Webware Application object.
 """
 
 from wkcommon import NoDefault
+import threading
+try:
+    from TaskKit.Scheduler import Scheduler
+except ImportError:
+    Scheduler = None
+
+_taskManager = None
+_makeTaskManagerLock = threading.Lock()
+
+def taskManager():
+    global _taskManager
+    if _taskManager is None:
+        if Scheduler is None:
+            assert 0, (
+                "FakeWebware is not installed, and/or TaskKit is "
+                "not available")
+        _makeTaskManagerLock.acquire()
+        try:
+            if _taskManager is None:
+                _taskManager = Scheduler(1)
+                _taskManager.start()
+        finally:
+            _makeTaskManagerLock.release()
+    return _taskManager
 
 class Application(object):
 
@@ -16,3 +40,6 @@ class Application(object):
     def setting(self, setting, default=NoDefault):
         assert default is not NoDefault, "No settings are defined"
         return default
+
+    def taskManager(self):
+        return taskManager()
