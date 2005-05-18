584cd2f9015a94800e33651805cb7a682c02fa87
Ian Bicking <ian@ianbicking.org>
2005-05-18 02:26:00 +0000

    Made prefix consistent with other servers (root_url), and make sure there's no trailing / (which breaks stuff)

--- paste/servers/scgi_server/__init__.py
+++ paste/servers/scgi_server/__init__.py
@@ -3,11 +3,14 @@ thirdparty.add_package('scgi')
 from scgiserver import serve_application
 
 def serve(conf, app):
-    prefix = conf.get('scgi_prefix', '/')
+    prefix = conf.get('root_url', '')
+    while prefix.endswith('/'):
+        prefix = prefix[:-1]
     serve_application(app, prefix, port=int(conf.get('port', 4000)))
 
 options = [
     ('port', 'Port to serve on (default 4000).'),
+    ('root_url', 'The URL level to expect for incoming connections; if not set and this is not bound to /, then SCRIPT_NAME and PATH_INFO may be incorrect.'),
     ]
 
 description = """\
