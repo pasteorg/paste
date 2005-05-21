a18f0c4899c5c0c46675c3505e783523c8cec331
Ian Bicking <ian@ianbicking.org>
2005-05-21 22:36:03 +0000

    Added build process for reference docs

--- paste/pyconfig.py
+++ paste/pyconfig.py
@@ -332,14 +332,14 @@ class DispatchingConfig(object):
         Thread-local configuration always overrides process-wide
         configuration.
 
-        This should be used like:
+        This should be used like::
 
-        conf = make_conf()
-        dispatching_config.push_thread_config(conf)
-        try:
-            ... do stuff ...
-        finally:
-            dispatching_config.pop_thread_config(conf)
+            conf = make_conf()
+            dispatching_config.push_thread_config(conf)
+            try:
+                ... do stuff ...
+            finally:
+                dispatching_config.pop_thread_config(conf)
         """
         local_dict().setdefault(self._local_key, []).append(conf)
 
