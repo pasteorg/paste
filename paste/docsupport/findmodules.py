e5b9cc078587ff866f6338b209cad967cbf48213
Ian Bicking <ian@ianbicking.org>
2005-05-22 06:24:33 +0000

    Only find files that are valid Python module names

--- paste/docsupport/findmodules.py
+++ paste/docsupport/findmodules.py
@@ -3,7 +3,10 @@ Finds all modules in a packages, loads them, and returns them.
 """
 
 import os
+import re
 from paste.util.import_string import import_module
+
+module_name = re.compile(r'^[a-zA-Z][a-zA-Z0-9_]*$')
 
 def find_modules(package):
     pkg_name = package.__name__
@@ -15,6 +18,8 @@ def find_modules(package):
         for module_fn in os.listdir(base):
             base, ext = os.path.splitext(module_fn)
             full = os.path.join(base, module_fn)
+            if not module_name.search(base):
+                continue
             if (os.path.isdir(full)
                 and os.path.exists(os.path.join(full, '__ini__.py'))):
                 modules.extend(import_module(pkg_name + '.' + base))
