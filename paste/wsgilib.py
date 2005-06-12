98bb1fa93e35d117a588a6542956877b7635a16c
Ian Bicking <ian@ianbicking.org>
2005-06-12 18:21:41 +0000

    Added function for parsing QUERY_STRING

--- paste/wsgilib.py
+++ paste/wsgilib.py
@@ -6,6 +6,7 @@ from Cookie import SimpleCookie
 from cStringIO import StringIO
 import mimetypes
 import os
+import cgi
 
 __all__ = ['get_cookies', 'add_close', 'raw_interactive',
            'interactive', 'construct_url', 'error_body_response',
@@ -27,6 +28,28 @@ def get_cookies(environ):
     cookies.load(header)
     environ['paste.cookies'] = (cookies, header)
     return cookies
+
+def parse_querystring(environ):
+    """
+    Parses a query string into a list like ``[(name, value)]``.
+    Caches this value in case parse_querystring is called again
+    for the same request.
+
+    You can pass the result to ``dict()``, but be aware that keys that
+    appear multiple times will be lost (only the last value will be
+    preserved).
+    """
+    source = environ.get('QUERY_STRING', '')
+    if not source:
+        return []
+    if 'paste.parsed_querystring' in environ:
+        parsed, check_source = environ['paste.parsed_querystring']
+        if check_source == source:
+            return parsed
+    parsed = cgi.parse_qs(source, keep_blank_values=True,
+                          strict_parsing=False)
+    environ['paste.parsed_querystring'] = (parsed, source)
+    return parsed
 
 class add_close:
     """
