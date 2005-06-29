ebc568f44865d8fd48454a43bc009267f5fe5134
ianb <ianb@localhost>
2005-06-29 23:11:15 +0000

    Made HTTPUnauthorized use a realm argument, so that it is compatible with Webware

--- paste/webkit/FakeWebware/WebKit/HTTPExceptions.py
+++ paste/webkit/FakeWebware/WebKit/HTTPExceptions.py
@@ -1,2 +1,8 @@
 from paste.httpexceptions import *
-HTTPAuthenticationRequired = HTTPUnauthorized
+
+class HTTPAuthenticationRequired(HTTPUnauthorized):
+
+    def __init__(self, realm=None, message=None, headers=None):
+        headers = headers or {}
+        headers['WWW-Authenticate'] = 'Basic realm=%s' % realm
+        HTTPUnathorized.__init__(self, message, headers)
