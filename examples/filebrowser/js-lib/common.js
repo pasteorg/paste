19c85898ba51a4084075db8a122c9b3f2e77cd68
ianb <ianb@localhost>
2005-07-06 09:06:43 +0000

    Added cookie functions; fixed Prototype extend change

--- examples/filebrowser/js-lib/common.js
+++ examples/filebrowser/js-lib/common.js
@@ -9,7 +9,7 @@ Element.hidden = function() {
 }
 
 function lazyToggle(container, url, options) {
-  options = ({method: 'get'}).extend(options);
+  options = extend({method: 'get'}, options || {});
   if (Element.hidden(container)) {
     Element.show(container);
     if (! $(container).lazy_loaded) {
@@ -54,4 +54,36 @@ function toggle(container, options) {
       }
     }
   }
+}
+
+/* From: http://www.quirksmode.org/js/cookies.html */
+
+function createCookie(name,value,days)
+{
+	if (days)
+	{
+		var date = new Date();
+		date.setTime(date.getTime()+(days*24*60*60*1000));
+		var expires = "; expires="+date.toGMTString();
+	}
+	else var expires = "";
+	document.cookie = name+"="+value+expires+"; path=/";
+}
+
+function readCookie(name)
+{
+	var nameEQ = name + "=";
+	var ca = document.cookie.split(';');
+	for(var i=0;i < ca.length;i++)
+	{
+		var c = ca[i];
+		while (c.charAt(0)==' ') c = c.substring(1,c.length);
+		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+	}
+	return null;
+}
+
+function eraseCookie(name)
+{
+	createCookie(name,"",-1);
 }
