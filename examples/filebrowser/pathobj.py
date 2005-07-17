4c44c5b19d5bc374a91d2a442cde88ed392d8144
ianb <ianb@localhost>
2005-07-17 22:47:53 +0000

    Added better HTML editing abilities

--- examples/filebrowser/pathobj.py
+++ examples/filebrowser/pathobj.py
@@ -1,6 +1,11 @@
 import re
 import os
 import mimetypes
+import itertools
+import random
+import urllib
+import cgi
+import htmlentitydefs
 from paste.wareweb import dispatch, public
 from paste.httpexceptions import *
 import sitepage
@@ -9,6 +14,13 @@ view_servlet_module = None
 
 class BadFilePath(Exception):
     pass
+
+def html_quote(v):
+    return cgi.escape(v, 1)
+def html_unquote(v, encoding='UTF8'):
+    for name, codepoint in htmlentitydefs.name2codepoint.iteritems():
+        v = v.replace('&%s;' % name, unichr(codepoint).encode(encoding))
+    return v
 
 class PathContext(object):
 
@@ -134,17 +146,16 @@ class Path(sitepage.SitePage):
         self.set_header('Content-type', self.mimetype)
         content = self.read()
         if (self.mimetype.startswith('text/html')
-            and servlet.fields.get('html') == 'clean'):
+            and self.fields.get('html') == 'clean'):
             for bad_regex in bad_regexes:
                 content = self.bad_regex.sub('', content)
         self.write(content)
 
     def action_save(self):
         content = self.fields.content
-        f = open(self.filename, 'wb')
-        f.write(content)
-        f.close()
-        self.message.write('%i bytes saved' % len(content))
+        self.write_text(content)
+        self.message.write('%i bytes saved to %s'
+                           % (len(content), self.basename))
         self.redirect(str(self.pathurl(action='view')))
 
     def read(self):
@@ -152,6 +163,11 @@ class Path(sitepage.SitePage):
         content = f.read()
         f.close()
         return content
+
+    def write_text(self, content):
+        f = open(self.filename, 'wb')
+        f.write(content)
+        f.close()
         
 PathContext.register_class(Path)
 
@@ -192,6 +208,129 @@ class HTMLFile(TextFile):
     def action_view(self):
         self.view = 'view_html.pt'
 
+    def action_edit(self):
+        super(HTMLFile, self).action_edit()
+        content = self.read()
+        head, body, tail = self.split_content(content)
+        self.options.content = body
+        props = self.read_properties(head)
+        op_props = self.options.properties = []
+        for name, value in sorted(props.items()):
+            op_props.append({
+                'description': name,
+                'input_name': 'property_' + name,
+                'type': 'text',
+                'value': value})
+
+    def action_save(self):
+        # Unlike Path.save, this doesn't save the entire contents
+        # of the file
+        content = self.fields.content
+        props = {}
+        for name, value in self.fields.items():
+            if name.startswith('property_'):
+                props[name[len('property_'):]] = value
+        self.save(content, props)
+        self.message.write('File %s saved' % self.basename)
+        self.redirect(str(self.pathurl(action='view')))
+
+    def save(self, content, props):
+        current = self.read()
+        head, body, tail = self.split_content(current)
+        new_head = self.save_properties(head, props)
+        new_content = new_head + content + tail
+        self.write_text(new_content)
+
+    def get_regexes(self, name, default=()):
+        regex = self.config.get(name)
+        if regex is None:
+            regex = []
+        if not isinstance(regex, (list, tuple)):
+            regex = [regex]
+        regex = list(regex)
+        if default:
+            regex.extend(default)
+        for i in range(len(regex)):
+            if isinstance(regex[i], (str, unicode)):
+                regex[i] = re.compile(regex[i], re.I)
+        return regex
+
+    def first_match(self, regexes, body):
+        for regex in regexes:
+            m = regex.search(body)
+            if m:
+                return m
+        return None
+
+    def split_content(self, content):
+        body_start_regex = self.get_regexes(
+            'body_start_regex', ['<body.*?>'])
+        body_end_regex = self.get_regexes(
+            'body_end_regex', ['</body>'])
+        m = self.first_match(body_start_regex, content)
+        if m:
+            head = content[:m.end()]
+            content = content[m.end():]
+        else:
+            head = ''
+        m = self.first_match(body_end_regex, content)
+        if m:
+            tail = content[m.start():]
+            content = content[:m.start()]
+        else:
+            tail = ''
+        return head, content, tail
+
+    title_re = re.compile('<title>(.*?)</title>', re.I|re.S)
+    def read_properties(self, head):
+        prop_regex = self.get_regexes(
+            'property_regex')
+        props = {}
+        m = self.title_re.search(head)
+        if m:
+            props['title'] = m.group(1)
+        for regex in prop_regex:
+            for match in regex.finditer(head):
+                groups = match.groupdict()
+                if 'value' in groups:
+                    value = groups['value']
+                elif 'html_value' in groups:
+                    value = html_unquote(groups['html_value'])
+                elif 'url_value' in groups:
+                    value = urllib.unquote(groups['url_value'])
+                props[match.group('name')] = value
+        return props
+
+    def save_properties(self, head, props):
+        prop_regex = self.get_regexes(
+            'property_regex')
+        prop_tmpl = self.config['property_template']
+        if 'title' in props:
+            title_html = '<title>%s</title>' % html_quote(props['title'])
+            m = self.title_re.search(head)
+            if m:
+                head = head[:m.start()] + title_html + head[m.end():]
+                del props['title']
+            else:
+                head = title_html + head
+        for name, value in props.items():
+            new_prop = prop_tmpl % {
+                'name': name,
+                'html_value': html_quote(value),
+                'url_value': urllib.quote(value),
+                'value': value}
+            for regex in prop_regex:
+                for match in regex.finditer(head):
+                    if name != match.group('name'):
+                        continue
+                    head = (head[:match.start()] + new_prop
+                            + head[match.end():])
+                    break
+                else:
+                    head = new_prop + head
+        return head
+        
+
 PathContext.register_class(HTMLFile)
 
 class Dir(Path):
@@ -199,6 +338,8 @@ class Dir(Path):
     extensions = ['dir']
 
     isdir = True
+
+    upload_id = itertools.count(random.randint(0, 15000))
 
     def setup_file(self):
         files = []
@@ -216,6 +357,10 @@ class Dir(Path):
             if path_servlet.isdir:
                 files[-1]['name'] += '/'
         self.options.files = files
+        self.options.upload_id = self.upload_id.next()
+        self.options.upload_url = self.globalurl(
+            'uploader', upload_id=self.options.upload_id,
+            redirect='/')
 
     def action_view_raw(self):
         self.action_view()
