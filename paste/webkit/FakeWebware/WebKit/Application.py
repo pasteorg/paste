82d331036f311709b77a1e3a90a9b191c9e49c61
Ian Bicking <ian@ianbicking.org>
2005-08-22 23:29:31 +0000

    Moved webkit to separate project

--- paste/webkit/FakeWebware/WebKit/Application.py
+++
@@ -1,1 +0,0 @@
-from paste.webkit.wkapplication import *
--- paste/webkit/FakeWebware/WebKit/HTTPExceptions.py
+++
@@ -1,8 +0,0 @@
-from paste.httpexceptions import *
-
-class HTTPAuthenticationRequired(HTTPUnauthorized):
-
-    def __init__(self, realm=None, message=None, headers=None):
-        headers = headers or {}
-        headers['WWW-Authenticate'] = 'Basic realm=%s' % realm
-        HTTPUnathorized.__init__(self, message, headers)
