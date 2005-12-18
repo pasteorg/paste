964ab5e51001c86d75f209c17d6af6c79199da7b
ianb <ianb@localhost>
2005-12-18 21:33:39 +0000

    Added deprecated module back in

---
+++ paste/printdebug.py
@@ -0,0 +1,9 @@
+# Deprecated 18 Dec 2005
+import warnings
+from paste.debug.prints import *
+
+warnings.warn(
+    "The paste.printdebug module has been moved to "
+    "paste.debug.prints",
+    DeprecationWarning, 2)
+
