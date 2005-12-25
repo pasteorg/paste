5933cab40690bee8add65b8dc65c08c5074aa09b
cce <cce@localhost>
2005-12-25 23:16:43 +0000

    Adding httpheaders, which is similar to httpexceptions only that it contains a full set of headers /w their corresponding category, version, and an indicator if they are single value or multi-value

---
+++ tests/test_httpheaders.py
@@ -0,0 +1,32 @@
+from paste.httpheaders import *
+
+def test_sorting():
+    # verify the HTTP_HEADERS are set with their canonical form
+    sample = [HTTP_WWW_AUTHENTICATE, HTTP_VIA, HTTP_ACCEPT, HTTP_DATE,
+    HTTP_ACCEPT_CHARSET, HTTP_AGE, HTTP_ALLOW, HTTP_CACHE_CONTROL,
+    HTTP_CONTENT_ENCODING, HTTP_ETAG, HTTP_CONTENT_TYPE, HTTP_FROM,
+    HTTP_EXPIRES, HTTP_RANGE, HTTP_UPGRADE, HTTP_VARY, HTTP_ALLOW]
+    sample.sort()
+    sample = [(x.category, x) for x in sample]
+    assert sample == [
+     # general headers first
+     ('general', 'Cache-Control'),
+     ('general', 'Date'),
+     ('general', 'Upgrade'),
+     ('general', 'Via'),
+     # request and response
+     ('request', 'Accept'),
+     ('request', 'Accept-Charset'),
+     ('response', 'Age'),
+     ('response', 'ETag'), # ETag is odd case
+     ('request', 'From'),
+     ('request', 'Range'),
+     ('response', 'Vary'),
+     ('response', 'WWW-Authenticate'), # so is WWW-Authenticate
+     # entity headers last
+     ('entity', 'Allow'), # expected duplicate
+     ('entity', 'Allow'),
+     ('entity', 'Content-Encoding'),
+     ('entity', 'Content-Type'),
+     ('entity', 'Expires')]
+
