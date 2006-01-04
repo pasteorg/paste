9e59f3d135a165df3be17c9414ac69ce3d79f2ec
Ian Bicking <ian@ianbicking.org>
2006-01-04 02:21:39 +0000

    Added docstrings to some modules

--- paste/debug/prints.py
+++ paste/debug/prints.py
@@ -1,5 +1,9 @@
 # (c) 2005 Ian Bicking and contributors; written for Paste (http://pythonpaste.org)
 # Licensed under the MIT license: http://www.opensource.org/licenses/mit-license.php
+"""
+Middleware that displays everything that is printed inline in
+application pages.
+"""
 
 from cStringIO import StringIO
 import re
