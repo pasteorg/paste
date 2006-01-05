3b0e667d40fb7f768fe751f948496a84bf01040a
cce <cce@localhost>
2006-01-05 10:52:02 +0000

    - its not important if pkg_resources is installed for paste;   it disables some features, but it doesn't depend upon this tool

--- paste/__init__.py
+++ paste/__init__.py
@@ -1,2 +1,6 @@
-import pkg_resources
-pkg_resources.declare_namespace('paste')
+try:
+    import pkg_resources
+    pkg_resources.declare_namespace('paste')
+except ImportError:
+    # don't prevent use of paste if pkg_resources isn't installed
+    pass
