45659f4c7702811c45d141ba0e14bfeb9e9d57f1
bbangert <bbangert@localhost>
2006-03-03 23:25:11 +0000

    Fixed invalid syntax, comma was missing

--- paste/error_document.py
+++ paste/error_document.py
@@ -1,6 +1,6 @@
 import warnings
 # Deprecated Mar 3 2006 (remove quickly: April 2006)
 warnings.warn(
-    'paste.error_document has been moved to paste.errordocument'
+    'paste.error_document has been moved to paste.errordocument',
     DeprecationWarning, 2)
 from paste.errordocument import *
