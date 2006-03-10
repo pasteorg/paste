dc17c0df397715955204c854994914fbdf2f8070
bbangert <bbangert@localhost>
2006-03-10 22:42:02 +0000

    Updating docs

--- paste/registry.py
+++ paste/registry.py
@@ -1,48 +1,47 @@
-"""Registry for Stacked
+"""Registry for handling request-local module globals sanely
 
-The Registry is intended to ensure that thread-local objects which are
-stacked are popped and pushed properly when a request is started and
-finished.
+Dealing with module globals in a thread-safe way is good if your
+application is the sole responder in a thread, however that approach fails
+to properly account for various scenarios that occur with WSGI applications
+and middleware.
 
-Thread-locals capable of being stacked are registered with the middleware
-and then tracked to ensure they're popped.
+What is actually needed in the case where a module global is desired that
+is always set properly depending on the current request, is a stacked
+thread-local object. Such an object is popped or pushed during the request
+cycle so that it properly represents the object that should be active for
+the current request.
 
-To ease creation of stacked thread-locals, an object with this interface
-is supplied.
-
-A stacked thread-local implements both thread-local access, and internally
-tracks the objects in a stack. The thread-local has objects pushed and
-popped from the stack to ensure that the correct object is being accessed
-for the scope of the request. 
-
-For convenience, the stacked thread-local can be used exactly like the
-object its tracking and will proxy all access through to the current
-object.
-
-An ideal use is for making request-specific objects available in a global
-manner while ensuring that when the object is accessed, the correct one is
-being used.
+To make it easy to deal with such variables, this module provides a special
+StackedObjectProxy class which you can instantiate and attach to your
+module where you'd like others to access it. The object you'd like this to
+actually "be" during the request is then registered with the
+RegistryManager middleware, which ensures that for the scope of the current
+WSGI application everything will work properly.
 
 Example::
     
-    #somemodule.py
-    myvar = StackedObjectProxy()
+    #yourpackage/__init__.py
+    
+    from paste.registry import RegistryManager, StackedObjectProxy
+    myglobal = StackedObjectProxy()
     
     #wsgi app stack
-    from paste.registry import RegistryManager
-    
-    app = RegistryManager(app)
+    app = RegistryManager(yourapp)
     
     #inside your wsgi app
     class yourapp(object):
         def __call__(self, environ, start_response):
-            obj = someobject  # Your object
+            obj = someobject  # The request-local object you want to access
+                              # via yourpackage.myglobal
             if environ.has_key('paste.registry'):
-                environ['paste.registry'].register(somemodule.myvar, obj)
+                environ['paste.registry'].register(myglobal, obj)
 
-You will then be able to import somemodule anywhere in your WSGI app or in
+You will then be able to import yourpackage anywhere in your WSGI app or in
 the calling stack below it and be assured that it is using the object you
 registered with Registry.
+
+RegisterManager can be in the WSGI stack multiple times, each time it
+appears it registers a new request context.
 
 """
 import paste.util.threadinglocal as threadinglocal
@@ -58,27 +57,20 @@ class StackedObjectProxy(object):
     where access to the object may be desired in multiple places without
     having to pass the actual object around.
     
-    New objects are added to the top of the stack with push_thread_object
-    while objects can be removed with pop_thread_object. 
+    New objects are added to the top of the stack with push_object while
+    objects can be removed with pop_object. 
     
     """
     def __init__(self):
+        """Create a new StackedObjectProxy"""
         self.__dict__['local'] = threadinglocal.local()
-        
-    def current_obj(self):
-        objects = getattr(self.__dict__['local'], 'objects', None)
-        if objects:
-            return objects[-1]
-        else:
-            raise TypeError(
-                "No object has been registered for this thread")
     
     def __getattr__(self, attr):
         return getattr(self.current_obj(), attr)
     
     def __setattr__(self, attr, value):
         setattr(self.current_obj(), name, value)
-        
+    
     def __delattr__(self, name):
         self.current_obj().__delattr__(name)
     
@@ -87,7 +79,7 @@ class StackedObjectProxy(object):
     
     def __setitem__(self, key, value):
         self.current_obj()[key] = value
-
+    
     def __delitem__(self, key):
         self.current_obj().__delitem__(key)
     
@@ -102,8 +94,17 @@ class StackedObjectProxy(object):
         # I thought __getattr__ would catch this, but apparently not
         return self.current_obj().has_key(key)
     
+    def current_obj(self):
+        """Returns the current active object being proxied to"""
+        objects = getattr(self.__dict__['local'], 'objects', None)
+        if objects:
+            return objects[-1]
+        else:
+            raise TypeError(
+                "No object has been registered for this thread")
+    
     def push_object(self, obj):
-        """Make ``obj`` the active object for this thread.
+        """Make ``obj`` the active object for this thread-local.
         
         This should be used like::
 
@@ -125,6 +126,7 @@ class StackedObjectProxy(object):
         
         If ``obj`` is given, it is checked against the popped object and an
         error is emitted if they don't match.
+        
         """
         if not hasattr(self.local, 'objects'):
             raise AssertionError("No object has been registered for this thread.")
@@ -137,28 +139,68 @@ class StackedObjectProxy(object):
                     % (popped, obj))
 
 class Registry(object):
-    """Track objects and stacked object proxies for removal"""
+    """Track objects and stacked object proxies for removal
+    
+    The Registry object is instantiated a single time for the rquest no
+    matter how many times the RegistryManager is used in a WSGI stack. Each
+    RegistryManager must call ``prepare`` before continuing the call to
+    start a new context for object registering.
+    
+    Each context is tracked with a dict inside a list. The last list
+    element is the currently executing context. Each context dict is keyed
+    by the id of the StackedObjectProxy instance being proxied, the value
+    is a tuple of the StackedObjectProxy instance and the object being
+    tracked.
+    
+    """
     def __init__(self):
+        """Create a new Registry object
+        
+        ``prepare`` must still be called before this Registry object can be
+        used to register objects.
+        
+        """
         self.reglist = []
     
     def prepare(self):
-        """Anytime a new RegistryManager comes in, it needs to add
-        objects for tracking using 
+        """Used to create a new registry context
+        
+        Anytime a new RegistryManager is called, ``prepare`` needs to be
+        called on the existing Registry object. This sets up a new context
+        for registering objects.
+        
         """
         self.reglist.append({})
     
     def register(self, stacked, obj):
+        """Register an object with a StackedObjectProxy"""
         stacked.push_object(obj)
         myreglist = self.reglist[-1]
         myreglist[id(stacked)] = (stacked, obj)
     
     def cleanup(self):
+        """Remove all objects from all StackedObjectProxy instances that
+        were tracked at this Registry context"""
         for id, val in self.reglist[-1].iteritems():
             stacked, obj = val
             stacked.pop_object(obj)
         self.reglist.pop()
         
 class RegistryManager(object):
+    """Creates and maintains a Registry context
+    
+    RegistryManager creates a new registry context for the registration of
+    StackedObjectProxy instances. Multiple RegistryManager's can be in a
+    WSGI stack and will manage the context so that the StackedObjectProxies
+    always proxy to the proper object.
+    
+    The object being registered can be any object sub-class, list, or dict.
+    
+    Registering objects is done inside a WSGI application under the
+    RegistryManager instance, using the ``environ['paste.registry']``
+    object.
+        
+    """
     def __init__(self, application):
         self.application = application
         
