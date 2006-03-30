b93f20b429faf6b6b829050c0cd4fd75ba8780f3
ianb <ianb@localhost>
2006-03-30 21:57:43 +0000

    Typo in not-found handler

--- paste/urlmap.py
+++ paste/urlmap.py
@@ -17,7 +17,7 @@ def urlmap_factory(loader, global_conf, **local_conf):
     else:
         not_found_app = global_conf.get('not_found_app')
     if not_found_app:
-        not_found_app = loader.get_app(app_name, global_conf=global_conf)
+        not_found_app = loader.get_app(not_found_app, global_conf=global_conf)
     urlmap = URLMap(not_found_app=not_found_app)
     for path, app_name in local_conf.items():
         path = parse_path_expression(path)
