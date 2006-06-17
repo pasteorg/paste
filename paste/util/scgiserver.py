60db75c877084a6117fd80c1e8b3c19df6286b48
pjenvey <pjenvey@localhost>
2006-06-17 22:30:59 +0000

    added optional host and max_children args to scgi's serve_application. Ideally the function prototype would match SCGIServers, i.e.:
    def serve_application(application, prefix, host=, port=None, max_children=None):
    or:
    def serve_application(application, prefix, **kwargs):
    
    (host, port and max_children could then default to SCGIServer's defaults),
    but that could break compatibility with those not explicitly using
    keyword args
    
    suggested by: greg@electricrain.com
    resolves: #55

--- paste/util/scgiserver.py
+++ paste/util/scgiserver.py
@@ -137,11 +137,37 @@ class SWAP(scgi_server.SCGIHandler):
             debug("IOError while closing connection ignored: %s" % err)
 
 
-def serve_application (application, prefix, port):
+def serve_application(application, prefix, port=None, host=None, max_children=None):
+    """
+    Serve the specified WSGI application via SCGI proxy.
+
+    ``application``
+        The WSGI application to serve.
+
+    ``prefix``
+        The prefix for what is served by the SCGI Web-server-side process.
+
+    ``port``
+        Optional port to bind the SCGI proxy to. Defaults to SCGIServer's
+        default port value.
+
+    ``host``
+        Optional host to bind the SCGI proxy to. Defaults to SCGIServer's
+        default host value.
+
+    ``host``
+        Optional maximum number of child processes the SCGIServer will
+        spawn. Defaults to SCGIServer's default max_children value.
+    """
     class SCGIAppHandler(SWAP):
         def __init__ (self, *args, **kwargs):
             self.prefix = prefix
             self.app_obj = application
             SWAP.__init__(self, *args, **kwargs)
 
-    scgi_server.SCGIServer(SCGIAppHandler, port=port).serve()
+    kwargs = dict(handler_class=SCGIAppHandler)
+    for kwarg in ('host', 'port', 'max_children'):
+        if locals()[kwarg] is not None:
+            kwargs[kwarg] = locals()[kwarg]
+
+    scgi_server.SCGIServer(**kwargs).serve()
