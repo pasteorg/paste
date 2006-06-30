dace17c17570d8ea50f4c2ea750d31ae3c77d14d
Ian Bicking <ian@ianbicking.org>
2006-06-30 01:20:37 +0000

    Patch from Brad Clements to add Paste Deploy support for paste.auth methods

--- paste/auth/basic.py
+++ paste/auth/basic.py
@@ -98,6 +98,25 @@ middleware = AuthBasicHandler
 
 __all__ = ['AuthBasicHandler']
 
+def make_basic(app, global_conf, realm, authfunc, **kw):
+    """
+    Grant access via basic authentication
+
+    Config looks like this::
+
+      [filter:grant]
+      use = egg:Paste#auth_basic
+      realm=myrealm
+      authfunc=somepackage.somemodule:somefunction
+      
+    """
+    from paste.util.import_string import eval_import
+    import types
+    authfunc = eval_import(authfunc)
+    assert isinstance(authfunc, types.FunctionType), "authfunc must resolve to a function"
+    return AuthBasicHandler(app, realm, authfunc)
+    
+
 if "__main__" == __name__:
     import doctest
     doctest.testmod(optionflags=doctest.ELLIPSIS)
