fe112fd4b081149ca69a6246d5529864ed97a1ed
ianb <ianb@localhost>
2006-10-20 19:05:40 +0000

    Moved evalexception entry point

--- paste/evalexception/middleware.py
+++ paste/evalexception/middleware.py
@@ -572,3 +572,21 @@ error_template = '''
 </body>
 </html>
 '''
+
+def make_eval_exception(app, global_conf, xmlhttp_key=None):
+    """
+    Wraps the application in an interactive debugger.
+
+    This debugger is a major security hole, and should only be
+    used during development.
+
+    xmlhttp_key is a string that, if present in QUERY_STRING,
+    indicates that the request is an XMLHttp request, and the
+    Javascript/interactive debugger should not be returned.  (If you
+    try to put the debugger somewhere with innerHTML, you will often
+    crash the browser)
+    """
+    if xmlhttp_key is None:
+        xmlhttp_key = global_conf.get('xmlhttp_key', '_')
+    return EvalException(app, xmlhttp_key=xmlhttp_key)
+
