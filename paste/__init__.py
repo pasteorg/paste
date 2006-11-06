1113615cfdc5ff09949795b89b555ca1343180fb
ianb <ianb@localhost>
2006-11-06 19:11:13 +0000

    Added modulefinder declaration, per Brad Clement's request

--- paste/__init__.py
+++ paste/__init__.py
@@ -7,3 +7,7 @@ except ImportError:
     # don't prevent use of paste if pkg_resources isn't installed
     from pkgutil import extend_path
     __path__ = extend_path(__path__, __name__) 
+
+import modulefinder
+for p in __path__:
+    modulefinder.AddPackagePath(__name__, p)
