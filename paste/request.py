e09da114e8ce0f05bd5aa8055dcbb3c7b86be90e
ianb <ianb@localhost>
2007-01-04 00:37:19 +0000

    Respect HTTP_HOST over SERVER_PORT in construct_url

--- paste/request.py
+++ paste/request.py
@@ -202,16 +202,15 @@ def construct_url(environ, with_query_string=True, with_path_info=True,
     url = environ['wsgi.url_scheme']+'://'
 
     if environ.get('HTTP_HOST'):
-        url += environ['HTTP_HOST'].split(':')[0]
+        url += environ['HTTP_HOST'].split(':')
     else:
         url += environ['SERVER_NAME']
-
-    if environ['wsgi.url_scheme'] == 'https':
-        if environ['SERVER_PORT'] != '443':
-            url += ':' + environ['SERVER_PORT']
-    else:
-        if environ['SERVER_PORT'] != '80':
-            url += ':' + environ['SERVER_PORT']
+        if environ['wsgi.url_scheme'] == 'https':
+            if environ['SERVER_PORT'] != '443':
+                url += ':' + environ['SERVER_PORT']
+        else:
+            if environ['SERVER_PORT'] != '80':
+                url += ':' + environ['SERVER_PORT']
 
     if script_name is None:
         url += urllib.quote(environ.get('SCRIPT_NAME',''))
