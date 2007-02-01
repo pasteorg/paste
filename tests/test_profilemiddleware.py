0caeb9eaeb4f2ca185a0c61b679addc4bee50933
Ian Bicking <ian@ianbicking.org>
2007-02-01 00:48:16 +0000

    don't test profile middleware if hotshot isn't available

--- tests/test_profilemiddleware.py
+++ tests/test_profilemiddleware.py
@@ -1,24 +1,29 @@
 from paste.fixture import *
-from paste.debug.profile import *
+try:
+    from paste.debug.profile import *
+    disable = False
+except ImportError:
+    disable = True
 
-def simple_app(environ, start_response):
-    start_response('200 OK', [('content-type', 'text/html')])
-    return ['all ok']
+if not disable:
+    def simple_app(environ, start_response):
+        start_response('200 OK', [('content-type', 'text/html')])
+        return ['all ok']
 
-def long_func():
-    for i in range(1000):
-        pass
-    return 'test'
+    def long_func():
+        for i in range(1000):
+            pass
+        return 'test'
 
-def test_profile():
-    app = TestApp(ProfileMiddleware(simple_app, {}))
-    res = app.get('/')
-    # The original app:
-    res.mustcontain('all ok')
-    # The profile information:
-    res.mustcontain('<pre')
-    
-def test_decorator():
-    value = profile_decorator()(long_func)()
-    assert value == 'test'
-    
+    def test_profile():
+        app = TestApp(ProfileMiddleware(simple_app, {}))
+        res = app.get('/')
+        # The original app:
+        res.mustcontain('all ok')
+        # The profile information:
+        res.mustcontain('<pre')
+
+    def test_decorator():
+        value = profile_decorator()(long_func)()
+        assert value == 'test'
+
