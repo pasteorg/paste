d0e575b4b3b4fe36f8209aac5685187e23d2d429
Ian Bicking <ian@ianbicking.org>
2007-02-01 00:32:22 +0000

    Remove long-deprecated modules (most moved elsewhere -- actually removing now); move mimeparse to paste.util.mimeparse

--- paste/error_document.py
+++
@@ -1,6 +0,0 @@
-import warnings
-# Deprecated Mar 3 2006 (remove quickly: April 2006)
-warnings.warn(
-    'paste.error_document has been moved to paste.errordocument',
-    DeprecationWarning, 2)
-from paste.errordocument import *
