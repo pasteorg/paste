172155fffa5190f5b26bed63e70f22aded27fa48
Ian Bicking <ian@ianbicking.org>
2007-03-06 03:53:14 +0000

    Added paste.util.import_string.try_import_module

--- paste/util/import_string.py
+++ paste/util/import_string.py
@@ -14,6 +14,14 @@ that case.
 """
 
 def eval_import(s):
+    """
+    Import a module, or import an object from a module.
+
+    A module name like ``foo.bar:baz()`` can be used, where
+    ``foo.bar`` is the module, and ``baz()`` is an expression
+    evaluated in the context of that module.  Note this is not safe on
+    arbitrary strings because of the eval.
+    """
     if ':' not in s:
         return simple_import(s)
     module_name, expr = s.split(':', 1)
@@ -22,6 +30,13 @@ def eval_import(s):
     return obj
 
 def simple_import(s):
+    """
+    Import a module, or import an object from a module.
+
+    A name like ``foo.bar.baz`` can be a module ``foo.bar.baz`` or a
+    module ``foo.bar`` with an object ``baz`` in it, or a module
+    ``foo`` with an object ``bar`` with an attribute ``baz``.
+    """
     parts = s.split('.')
     module = import_module(parts[0])
     name = parts[0]
@@ -46,8 +61,35 @@ def simple_import(s):
     return obj
 
 def import_module(s):
+    """
+    Import a module.
+    """
     mod = __import__(s)
     parts = s.split('.')
     for part in parts[1:]:
         mod = getattr(mod, part)
     return mod
+
+def try_import_module(module_name):
+    """
+    Imports a module, but catches import errors.  Only catches errors
+    when that module doesn't exist; if that module itself has an
+    import error it will still get raised.  Returns None if the module
+    doesn't exist.
+    """
+    try:
+        return import_module(module_name)
+    except ImportError, e:
+        if not getattr(e, 'args', None):
+            raise
+        desc = e.args[0]
+        if not desc.startswith('No module named '):
+            raise
+        desc = desc[len('No module named '):]
+        # If you import foo.bar.baz, the bad import could be any
+        # of foo.bar.baz, bar.baz, or baz; we'll test them all:
+        parts = module_name.split('.')
+        for i in range(len(parts)):
+            if desc == '.'.join(parts[i:]):
+                return None
+        raise
