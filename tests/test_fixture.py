18336eaff1feed6aedcc4a9d5c1ebb74bfda45b4
ianb <ianb@localhost>
2007-05-25 02:46:34 +0000

    Added REST methods to Test, from Anders Pearson

--- tests/test_fixture.py
+++ tests/test_fixture.py
@@ -6,4 +6,10 @@ def test_fixture():
     res = app.get('/', params={'a': ['1', '2']})
     assert (res.request.environ['QUERY_STRING'] ==
             'a=1&a=2')
+    res = app.put('/')
+    assert (res.request.environ['REQUEST_METHOD'] ==
+            'PUT')
+    res = app.delete('/')
+    assert (res.request.environ['REQUEST_METHOD'] ==
+            'DELETE')
     
