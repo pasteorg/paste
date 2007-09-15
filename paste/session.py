5b3449980e81eeec99770daf2cbc2dd7ba98ac6a
Ian Bicking <ian@ianbicking.org>
2007-09-15 17:53:56 +0000

    In paste.session, ignore files that aren't session files

--- paste/session.py
+++ paste/session.py
@@ -235,13 +235,17 @@ class FileSession(object):
         if len(t) != 2:
             return
         t = t[0]
-        sess_time = datetime.datetime(
-                int(t[0:4]),
-                int(t[4:6]),
-                int(t[6:8]),
-                int(t[8:10]),
-                int(t[10:12]),
-                int(t[12:14]))
+        try:
+            sess_time = datetime.datetime(
+                    int(t[0:4]),
+                    int(t[4:6]),
+                    int(t[6:8]),
+                    int(t[8:10]),
+                    int(t[10:12]),
+                    int(t[12:14]))
+        except ValueError:
+            # Probably not a session file at all
+            return
 
         if sess_time + exp_time < now:
             os.remove(os.path.join(self.session_file_path, f))
