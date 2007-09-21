22da539f8f6017dee24aaa63ebaec2a67ff8ffe9
ianb <ianb@localhost>
2007-09-21 03:29:04 +0000

    Improve performance of HeaderDict.headeritems()

--- paste/response.py
+++ paste/response.py
@@ -68,12 +68,12 @@ class HeaderDict(dict):
 
     def headeritems(self):
         result = []
-        for key in self:
-            if isinstance(self[key], list):
-                for v in self[key]:
+        for key, value in self.items():
+            if isinstance(value, list):
+                for v in value:
                     result.append((key, str(v)))
             else:
-                result.append((key, str(self[key])))
+                result.append((key, str(value)))
         return result
 
     #@classmethod
