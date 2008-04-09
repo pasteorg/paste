896ce1ce97256f931959dcf05606ccb75c3de3eb
Ian Bicking <ian@ianbicking.org>
2008-04-09 17:01:24 +0000

    Make modulefinder import conditional

--- paste/__init__.py
+++ paste/__init__.py
@@ -8,6 +8,10 @@ except ImportError:
     from pkgutil import extend_path
     __path__ = extend_path(__path__, __name__) 
 
-import modulefinder
-for p in __path__:
-    modulefinder.AddPackagePath(__name__, p)
+try:
+    import modulefinder
+except ImportError:
+    pass
+else:
+    for p in __path__:
+        modulefinder.AddPackagePath(__name__, p)
