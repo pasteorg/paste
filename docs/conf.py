2521414e4c36d1e2900e794d112a58f3a28dfbb7
Ian Bicking <ianb@colorstudy.com>
2010-04-20 12:05:12 -0500

    update versions for release

--- docs/conf.py
+++ docs/conf.py
@@ -42,7 +42,7 @@ copyright = '2008, Ian Bicking'
 # The short X.Y version.
 version = '1.7'
 # The full version, including alpha/beta/rc tags.
-release = '1.7.3'
+release = '1.7.3.1'
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
