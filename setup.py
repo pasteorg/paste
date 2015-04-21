981e67acd99cc2c57e8d9b67b3248e986cfb3967
Victor Stinner <victor.stinner@gmail.com>
2015-04-22 01:41:53 +0200

    Prepare release 2.0

--- setup.py
+++ setup.py
@@ -4,22 +4,22 @@
 # - update version in setup.py (__version__)
 # - update changelog: docs/news.txt
 # - modify setup.py: set RELEASE to True
-#  - check that "python setup.py sdist" contains all files tracked by
-#    the SCM (Mercurial): update MANIFEST.in if needed
+# - check that "python setup.py sdist" contains all files tracked by
+#   the SCM (Mercurial): update MANIFEST.in if needed
 #
-#  - hg ci
-#  - hg tag VERSION
-#  - hg push
-#  - python setup.py register sdist bdist_wheel upload
+# - hg ci
+# - hg tag VERSION
+# - hg push
+# - python setup.py register sdist bdist_wheel upload
 #
-#  - increment version in setup.py (__version__)
-#  - hg ci && hg push
+# - increment version in setup.py (__version__)
+# - hg ci && hg push
 
 # If true, then the svn revision won't be used to calculate the
 # revision (set to True for real releases)
-RELEASE = False
+RELEASE = True
 
-__version__ = '1.7.6'
+__version__ = '2.0'
 
 from setuptools import setup, find_packages
 import sys, os
