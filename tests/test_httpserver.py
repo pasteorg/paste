0fe289bf2f016ab296b17e47ef2c94ec38ed025d
Marc Abramowitz <marc@marc-abramowitz.com>
2015-04-27 16:52:56 -0700

    Add tests/test_httpserver.py
    which contains a test for the issue in BB-4, where the WSGI environment has
    strings with commas in them that don't belong.
    
    See issue #4.

---
+++ tests/test_httpserver.py
@@ -0,0 +1,29 @@
+import mimetools
+
+from paste.httpserver import WSGIHandler
+from six.moves import StringIO
+
+
+class MockServer(object):
+    server_address = ('127.0.0.1', 80)
+
+
+class MockSocket(object):
+    def makefile(self, mode, bufsize):
+        return StringIO()
+
+
+def test_environ():
+    mock_socket = MockSocket()
+    mock_client_address = '1.2.3.4'
+    mock_server = MockServer()
+
+    wsgi_handler = WSGIHandler(mock_socket, mock_client_address, mock_server)
+    wsgi_handler.command = 'GET'
+    wsgi_handler.path = '/path'
+    wsgi_handler.request_version = 'HTTP/1.0'
+    wsgi_handler.headers = mimetools.Message(StringIO('Host: mywebsite'))
+
+    wsgi_handler.wsgi_setup()
+
+    assert wsgi_handler.wsgi_environ['HTTP_HOST'] == 'mywebsite'
